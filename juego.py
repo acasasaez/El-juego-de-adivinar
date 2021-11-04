3c0cdfc204ea445de02de4214dec83f1a6cb580d
acasasaez <acasasae@myuax.com>
2021-11-04 14:18:37 +0100

    primera condición

--- juego.py
+++ juego.py
@@ -1,6 +1,9 @@
 import random
 numero = random.randint(0,99)
-
 intento = int (input ("Elige un número del 0 al 99: "))
 número_intento= 0
 while intento != número_intento:
+    if intento < numero:
+        print ("Demasiado pequeño")
+        número_intento = número_intento + 1
+        intento = int (input ("Elige un número del 0 al 99: "))
