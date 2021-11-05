db4f31c0a5f03962143ad520066014e6ff3994ca
acasasaez <acasasae@myuax.com>
2021-11-05 08:45:37 +0100

    otro cambio

--- juego.py
+++ juego.py
@@ -1,11 +1,11 @@
 import random
 numero = random.randint(0,99)
 intento = int (input ("Elige un número del 0 al 99: "))
-número_intento= 0
-while intento != número_intento:
+numero_intento= 0
+while intento != numero_intento:
     if intento < numero:
         print ("Demasiado pequeño")
-        número_intento = número_intento + 1
+        numero_intento = numero_intento + 1
         intento = int (input ("Elige un número del 0 al 99: "))
     if intento > numero:
         print ("Demasiado grande")
